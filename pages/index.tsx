c3d14cb396b7aac6eb8c6cbd155585e801f1d8a2
gary <yiso05255@gmail.com>
2021-08-08 17:37:24 +0800

    add styled-components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,23 @@
 import styles from '../styles/Home.module.css'
+import styled, { css } from 'styled-components'
 
 export default function Home() {
+  const Button = styled.button`
+    border-radius: 3px;
+    border: 2px solid palevioletred;
+    color: palevioletred;
+    margin: 0.5em 1em;
+    padding: 0.25em 1em;
+
+    ${props =>
+      props.primary &&
+      css`
+        background: palevioletred;
+        color: white;
+      `}
+  `
   return (
-    <div className={styles.container}>
+    <div className={styles.container + ' p-8'}>
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
@@ -10,8 +25,9 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>登入</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h2>Empty Project</h2>
+            <Button>Normal Button</Button>
+            <Button primary>Primary Button</Button>
           </a>
         </div>
       </main>
